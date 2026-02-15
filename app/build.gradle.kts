8b85ec4c47a97d88e5b5710feaf3e4e75fc93879
alef <alexeykrasilnikov76@gmail.com>
2026-02-15 16:16:15 +0300

    upgrade deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,21 +83,21 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
-    implementation("androidx.activity:activity-compose:1.12.2")
-    implementation(platform("androidx.compose:compose-bom:2026.01.00"))
+    implementation("androidx.activity:activity-compose:1.12.4")
+    implementation(platform("androidx.compose:compose-bom:2026.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
-    implementation("androidx.navigation:navigation-compose:2.9.6")
+    implementation("androidx.navigation:navigation-compose:2.9.7")
     implementation("io.coil-kt.coil3:coil-compose:3.3.0")
     implementation("com.google.accompanist:accompanist-permissions:0.37.3")
 
     // DI
-    implementation("com.google.dagger:hilt-android:2.59")
+    implementation("com.google.dagger:hilt-android:2.59.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
-    ksp("com.google.dagger:hilt-android-compiler:2.59")
+    ksp("com.google.dagger:hilt-android-compiler:2.59.1")
 
     // Room
     implementation("androidx.room:room-runtime:$roomVersion")
@@ -105,14 +105,14 @@ dependencies {
     ksp("androidx.room:room-compiler:$roomVersion")
 
     // KtLint
-    ktlintRuleset("io.nlopez.compose.rules:ktlint:0.5.5")
+    ktlintRuleset("io.nlopez.compose.rules:ktlint:0.5.6")
 
     // Test
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2026.01.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2026.02.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.10.1")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.10.3")
 }
