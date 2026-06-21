67dcc0b1c5a3e6da202da4ee4b758878569cbe16
alef <alexeykrasilnikov76@gmail.com>
2026-06-21 12:37:59 +0300

    floating navbar

--- app/src/main/java/com/asinosoft/gallery/ui/MainView.kt
+++ app/src/main/java/com/asinosoft/gallery/ui/MainView.kt
@@ -1,34 +1,49 @@
 package com.asinosoft.gallery.ui
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animate
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.asinosoft.gallery.R
@@ -36,6 +51,7 @@ import com.asinosoft.gallery.data.Album
 import com.asinosoft.gallery.data.Media
 import com.asinosoft.gallery.model.ImageListViewModel
 import com.asinosoft.gallery.ui.component.FilterDialog
+import com.asinosoft.gallery.ui.theme.GalleryTheme
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -54,31 +70,59 @@ fun MainView(
     val topScroll = TopAppBarDefaults.enterAlwaysScrollBehavior()
     val showFilters = remember { mutableStateOf(false) }
 
-    Scaffold(modifier) { paddingValues ->
+    var navbarHeight by remember { mutableFloatStateOf(0f) }
+    var navbarOffset by remember { mutableFloatStateOf(0f) }
+    val navbarScrollConnection = remember {
+        object : NestedScrollConnection {
+            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+                val delta = available.y
+                val newOffset = navbarOffset - delta
+                navbarOffset = newOffset.coerceIn(0f, navbarHeight)
+                return Offset.Zero
+            }
+
+            override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
+                val targetOffset = if (navbarOffset > navbarHeight/2f) navbarHeight else 0f
+                coroutineScope.launch {
+                    animate(
+                        initialValue = navbarOffset,
+                        targetValue = targetOffset,
+                        initialVelocity = 10f
+                    ) { y, _ ->
+                        navbarOffset = y
+                    }
+                }
+
+                return super.onPostFling(consumed, available)
+            }
+        }
+    }
+
+    Scaffold(modifier.nestedScroll(navbarScrollConnection)) { paddingValues ->
         PullToRefreshBox(
-            isRefreshing = isFetching,
-            onRefresh = model::fetch,
-            Modifier.padding(paddingValues)
+            isRefreshing = isFetching, onRefresh = model::fetch, Modifier.padding(paddingValues)
         ) {
             Column {
                 AnimatedVisibility(visible = selection.isEmpty()) {
                     TopAppBar(
-                        title = { },
-                        actions = {
+                        title = { }, actions = {
                             if (selection.isEmpty() && 0 == pagerState.currentPage) {
-                                IconButton(onClick = { showFilters.value = true}) {
-                                    Icon(painterResource(R.drawable.filter), contentDescription = null)
+                                IconButton(onClick = { showFilters.value = true }) {
+                                    Icon(
+                                        painterResource(R.drawable.filter),
+                                        contentDescription = null
+                                    )
                                 }
                             }
-                            IconButton(onClick = onSettingsClick ) {
-                                Icon(painterResource(R.drawable.settings), contentDescription = null)
+                            IconButton(onClick = onSettingsClick) {
+                                Icon(
+                                    painterResource(R.drawable.settings), contentDescription = null
+                                )
                             }
-                        },
-                        colors = TopAppBarDefaults.topAppBarColors(
+                        }, colors = TopAppBarDefaults.topAppBarColors(
                             containerColor = Color.Transparent,
                             scrolledContainerColor = Color.Transparent
-                        ),
-                        scrollBehavior = topScroll
+                        ), scrollBehavior = topScroll
                     )
                 }
 
@@ -99,13 +143,14 @@ fun MainView(
             }
 
             AnimatedVisibility(
-                visible = selection.isEmpty(),
-                modifier = Modifier.align(Alignment.BottomCenter)
+                visible = selection.isEmpty(), modifier = Modifier.align(Alignment.BottomCenter)
             ) {
                 ViewModeBar(
                     pagerState = pagerState,
                     onPhotos = { coroutineScope.launch { pagerState.scrollToPage(0) } },
                     onAlbums = { coroutineScope.launch { pagerState.scrollToPage(1) } },
+                    modifier.onGloballyPositioned { navbarHeight = it.size.height.toFloat() }
+                        .offset { IntOffset(0, navbarOffset.toInt()) }
                 )
             }
 
@@ -114,47 +159,78 @@ fun MainView(
                 FilterDialog(
                     filters = filters,
                     onChangeFilter = model::setFilter,
-                    onDismiss = { showFilters.value = false }
-                )
+                    onDismiss = { showFilters.value = false })
             }
         }
     }
 }
 
 @Composable
-fun ViewModeBar(
+private fun ViewModeBar(
     pagerState: PagerState,
     onPhotos: () -> Unit,
     onAlbums: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    NavigationBar(
-        modifier = modifier,
-        containerColor = Color.White.copy(0.8f)
+    Surface(
+        modifier = modifier.padding(16.dp),
+        shape = RoundedCornerShape(50),
+        color = MaterialTheme.colorScheme.surface.copy(alpha = .8f),
     ) {
-        Spacer(Modifier.width(60.dp))
-        NavigationBarItem(
-            selected = 0 == pagerState.currentPage,
-            onClick = onPhotos,
-            icon = {
+        val size = Modifier.width(96.dp)
+        Row(Modifier.padding(2.dp)) {
+            EllipseButton(
+                onClick = onPhotos,
+                selected = 0 == pagerState.currentPage,
+                icon = painterResource(R.drawable.photo),
+                label = stringResource(R.string.photos),
+                modifier = size
+            )
+
+            EllipseButton(
+                onClick = onAlbums,
+                selected = 1 == pagerState.currentPage,
+                icon = painterResource(R.drawable.album),
+                label = stringResource(R.string.albums),
+                modifier = size
+            )
+        }
+    }
+}
+
+@Composable
+private fun EllipseButton(
+    onClick: () -> Unit,
+    selected: Boolean,
+    icon: Painter,
+    label: String,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(50),
+        color = if (selected) MaterialTheme.colorScheme.onPrimary.copy(alpha = .8f) else Color.Transparent
+    ) {
+        TextButton(
+            onClick = onClick,
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 Icon(
-                    painter = painterResource(R.drawable.photo),
-                    contentDescription = stringResource(id = R.string.photos)
+                    painter = icon, contentDescription = label
                 )
-            },
-            label = { Text(stringResource(id = R.string.photos)) }
-        )
-        NavigationBarItem(
-            selected = 1 == pagerState.currentPage,
-            onClick = onAlbums,
-            icon = {
-                Icon(
-                    painter = painterResource(R.drawable.album),
-                    contentDescription = stringResource(id = R.string.albums)
+                Text(
+                    text = label, style = MaterialTheme.typography.labelSmall
                 )
-            },
-            label = { Text(stringResource(id = R.string.albums)) }
-        )
-        Spacer(Modifier.width(60.dp))
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    GalleryTheme {
+        val pagerState = rememberPagerState { 2 }
+        ViewModeBar(pagerState, {}, {})
     }
 }
